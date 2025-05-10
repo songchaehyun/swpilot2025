d09c2eec33afc182572717cb45a4c2b4600b4cef
songchaehyun <songchaehyun@knu.ac.kr>
2025-05-10 10:25:00 +0900

    1-1 과제 완료

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Hello Mars")
+
+try :
+    try :
+        with open("1-1-mission_computer_main.log", 'r') as f1, open("problem.log", "w") as f2:
+            #보너스 과제 : 출력 결과를 시간의 역순으로 정렬해서 출력
+            data = f1.readlines()
+            print("-"*5 + f1.name +"-"*5)
+            print(data[0].strip())
+            for item in reversed(data[1:]) : #중복 출력을 방지하기 위해 리스트 슬라이싱 사용.
+                print(item.strip())
+            print("\n")
+            
+            for item in data[-3:] : #보너스 과제 : 문제가 되는 부분만 따로 파일로 저장
+                f2.write(item)
+    except FileNotFoundError:
+        print("Error occur: file not found")
+    
+    # 인코딩을 지정하지 않아 log_analysis.md 파일을 vscode로 열었을 시 UTF-8 규격과 맞지 않아 문제 발생.
+    with open("log_analysis.md", "w", encoding="utf-8") as mymd :
+        analysis = "로켓 발사의 분석\n11:30:00경 미션이 성공적으로 complete되었음.\n그러나 11:35:00경 산소탱크에 문제가 발생함.\n이후 산소 탱크가 폭발하며 핵심 시스템이 다운됨."
+        mymd.write(analysis)
+        mymd.close()
+    
+    with open("log_analysis.md", "r", encoding="utf-8") as f:
+        txt = f.read()
+        print("-"*5 + f.name +"-"*5)
+        print(txt)
+        print("\n")
+        f.close()
+
+except Exception as e:
+    print(f"Error occur: {e}")
